6c0d8457f99030653618ffd963e4e34f993e473c
novakvova <novakvova@gmail>
2023-06-05 16:14:15 +0300

    Working app is good

---
+++ local.pd123.com/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Мій сайт PHP</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/site.css">
+</head>
+<body>
+<?php include $_SERVER["DOCUMENT_ROOT"]."/header.php"; ?>
+<main>
+    <div class="container">
+        <h1>Привіт PHP</h1>
+        <?php echo "<h2>Я PHP</h2>"; ?>
+    </div>
+</main>
+
+
+    <script src="/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>

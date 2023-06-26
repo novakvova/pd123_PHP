b21ac0d2d14ae6782b41abefa77284301a88bdbf
novakvova <novakvova@gmail>
2023-06-26 16:25:36 +0300

    Begin workign sidebar

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,24 +4,29 @@ import {Route, Routes} from "react-router-dom";
 import CategoryListPage from "./components/category/list/CategoryListPage";
 import CategoryCreatePage from "./components/category/create/CategoryCreatePage";
 import CategoryEditPage from "./components/category/edit/CategoryEditPage";
+import AdminHomePage from "./components/admin/home/AdminHomePage";
+
 const App = () => {
 
-  return (
-    <>
-      <Routes>
-        <Route path="/" >
-          <Route index element={<CategoryListPage/>}/>
-            <Route path={"/category"}>
-                <Route path="create" element={<CategoryCreatePage/>}/>
-                <Route path="edit">
-                    <Route path=":id" element={<CategoryEditPage/>} />
-                </Route>
-            </Route>
+    return (
+        <>
+            <Routes>
+                <Route path="/">
+                    <Route index element={<CategoryListPage/>}/>
+                    <Route path={"/category"}>
+                        <Route path="create" element={<CategoryCreatePage/>}/>
+                        <Route path="edit">
+                            <Route path=":id" element={<CategoryEditPage/>}/>
+                        </Route>
+                    </Route>
 
-        </Route>
-      </Routes>
-    </>
-  );
+                </Route>
+                <Route path={"admin"}>
+                    <Route index element={<AdminHomePage/>}/>
+                </Route>
+            </Routes>
+        </>
+    );
 };
 
 export default App;
---
+++ my-app/src/components/admin/home/AdminHomePage.tsx
@@ -0,0 +1,14 @@
+import AdminSidebar from "./AdminSidebar";
+import "./admin.scss";
+
+const AdminHomePage = () => {
+    return (
+        <>
+            <div className={"admin"}>
+                <AdminSidebar/>
+            </div>
+        </>
+    )
+}
+
+export default AdminHomePage;
